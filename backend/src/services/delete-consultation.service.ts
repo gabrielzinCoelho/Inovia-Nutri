8a821d389af6358ccdc0ea47f8571806b59fec0c
Gabriel Coelho Costa <coelhocostag@gmail.com>
2024-12-14 11:34:54 -0300

    frontend: setup inicial e adicionado estilização global com styled components

--- backend/src/services/delete-consultation.service.ts
+++ backend/src/services/delete-consultation.service.ts
@@ -20,7 +20,7 @@ export class DeleteConsultationService {
     const consultation = await this.consultationModel.findByIdAndDelete(id)
 
     if (!consultation)
-      throw new BadRequestException('Consultation dont exists.')
+      throw new BadRequestException('Consultation does not exist.')
 
     return consultation
   }
