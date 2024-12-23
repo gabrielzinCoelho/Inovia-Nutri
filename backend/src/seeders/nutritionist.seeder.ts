8226e0c8c6ca78c04e0b4b13b493b9794c151eb1
Gabriel Coelho Costa <coelhocostag@gmail.com>
2024-12-23 11:52:26 -0300

    seeding do banco de dados na execução do container

--- backend/src/seeders/nutritionist.seeder.ts
+++ backend/src/seeders/nutritionist.seeder.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { DataFactory, Seeder } from 'nestjs-seeder'
 import { Nutritionist } from 'src/schemas/nutritionist.schema'
-
+import { hash } from 'bcryptjs'
 @Injectable()
 export class NutritionistSeeder implements Seeder {
   constructor(
@@ -14,7 +14,16 @@ export class NutritionistSeeder implements Seeder {
   async seed(): Promise<any> {
     const seededNutritionists =
       DataFactory.createForClass(Nutritionist).generate(2)
-    return await this.nutritionistModel.insertMany(seededNutritionists)
+
+    const defaultNutritionist = new this.nutritionistModel({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('12345', 6),
+    })
+    return await this.nutritionistModel.insertMany([
+      ...seededNutritionists,
+      defaultNutritionist,
+    ])
   }
 
   async drop(): Promise<any> {
